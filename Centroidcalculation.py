35f60ecf22188341e57dbc5d8689ec90ec06a17d
mdheer <37894626+mdheer@users.noreply.github.com>
2019-02-18 16:22:41 +0100

    file creation

---
+++ Centroidcalculation.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 15:49:34 2019
+
+@author: Mathieu D'heer
+"""
+
+import UniversalConstants as UC
+from math import *
---
+++ MomentOfInertia.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 15:49:34 2019
+
+@author: Mathieu D'heer
+"""
+
+import UniversalConstants as UC
+from math import *
+
+
+
+def MomentOfInertia(Boomdiscretization)
