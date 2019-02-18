d25cde4983749595048b49dbd0141fba1ebbf453
tblaha <till.blaha@web.de>
2019-02-18 17:47:06 +0100

    Very work in progress start on the boom discretization
    trickier than I thought since I need to make a tradeoff between the "equal lengths" condition and fitting them into the cells. Will be finished before wednesday

--- discretization.py
+++ discretization.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Author: tblaha
+Date  : 2019-02-18
 """
 
+from UniversalConstants import *
+import numpy as np
 
-def discretizeCrossSetion(number_of_stiffeners, booms_between):
+def discretizeCrossSection(h_a, c_a, n_st, booms_between):
     # Takes the geometry, the number of stiffeners and the number of booms in
     # between each stiffener and compute boom locations and areas
+    #
     # --- INPUTS --- #
-    # !!! some more geometric parameters will be added in the future !!!
-    # number_of_stiffeners: The amount of equally spaced stiffeners along the
-    #                       perimiter of the skin panels
+    # h_a                 : height of aileron (float)
+    # c_a                 : chord length of aileron (float)
+    # n_st                : The amount of equally spaced stiffeners along the
+    #                       perimiter of the skin panels (int)
     # booms_between       : The amount of booms in between stiffeners; 
-    #                       stiffeners themselves are automatically booms
-    
+    #                       stiffeners themselves are automatically booms (int)
     # --- OUTPUTS --- #
     # 2d numpy array:
     # | y-loc | z-loc | area if y-bending | area if z-bending | cell id |
@@ -26,6 +28,35 @@ def discretizeCrossSetion(number_of_stiffeners, booms_between):
     #          2 (exclusively right cell, clockwise sorted)
     #          3 (exclusively spar, downwards sorted)
     
+    # we know that there are n_st+booms_between*(n_st+1) booms, so we can
+    # iterate over that range and figure out each boom
+    Bs = np.zeros((n_st+booms_between*(n_st+1), 5))
+    
+    # Find total arc length of the semi circle
+    sc_arc_length = h_a*np.pi/2
+    
+    # Total length of the 2 straight sections
+    straight_length = 2*np.sqrt(h_a**2 + (c_a-h_a)**2)
+    
+    # Total length
+    total_length = sc_arc_length + straight_length
+    
+    # length per segment
+    length_per_segment = total_length/(n_st+1)
+    
+    # stiffeners in the circular segment
+    stiffs_circular = np.floor(sc_arc_length/length_per_segment)
+    
+    # booms in circular section
+    for i in range(int(stiffs_circular)):
+        unit_angle = (2 - i) * length_per_segment/(2*sc_arc_length) * 2*np.pi;
+        Bs[i,0]    = np.sin(unit_angle)*h_a/2
+        Bs[i,1]    = np.cos(unit_angle)*h_a/2
+        
+        
+        
+    return  Bs
+        
     
     
 def discretizeSpan(x_h1, x_h2, x_h3, d_a, l_a, nodes_between):
@@ -34,6 +65,7 @@ def discretizeSpan(x_h1, x_h2, x_h3, d_a, l_a, nodes_between):
     # will be computed later. Spanwise nodes (the x-location of the discrete
     # cross sections) are closer to spanwise items like ribs and actuators,
     # since a lot of the stresses
+    #
     # --- INPUTS --- #
     # x_h1, x_h2, x_h3    : locations of the hinges
     # d_a                 : distance between the actuators; centred in hinge 2
@@ -41,7 +73,6 @@ def discretizeSpan(x_h1, x_h2, x_h3, d_a, l_a, nodes_between):
     # nodes_between       : needs to be a positive, even number! The spanwise
     #                       nodes in between two spanwise "events"
     #                       (rib, hinge and one of the two ends)
-    
     # --- OUTPUTS --- #
     # 1d numpy array: of the x-locations (NOT equally spaced)
     # | x-loc |
@@ -49,5 +80,9 @@ def discretizeSpan(x_h1, x_h2, x_h3, d_a, l_a, nodes_between):
     # | 15.5  |
     # | ...   |
     
+    return 0
     
+
+
+Bs = discretizeCrossSection(h_a, c_a, n_st, 2)
     
