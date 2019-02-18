40a6bebb42782bb747fa3c642b9d818150dc2a3a
DaanVerkooij <47591778+DaanVerkooij@users.noreply.github.com>
2019-02-18 14:16:34 +0100

    Create UniversalConstants.py

---
+++ UniversalConstants.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 14:04:53 2019
+
+@author: daanv
+"""
+#All in metres, N, N/m, Deg
+Ca=0.547    #Chord Length Aileron,          0.547m
+La=2.771    #Length of the aileron,         2.771m
+xh1=0.153   #X-pos Hinge 1,                 0.153m
+xh2=1.281   #X-pos Hinge 2,                 1.281m
+xh3=2.681   #X-pos Hinge 3,                 2.681m
+da=0.28     #Distance between ailerons,     28cm
+ha=0.225    #Height Aileron,                22.5cm
+tsk=0.0011  #Thickness Skin,                1.1mm
+tsp=0.0029  #Thickness Spar,                2.9mm
+tst=0.0012  #Thickness Stiffener,           1.2mm
+hst=0.015   #Height Stiffener,              1.5cm
+wst=0.02    #Width Stiffener,               2cm
+nst=17      #Number of Stiffeners,          17
+d1=0.01103  #Vertical displacement hinge 1, 1.103cm
+d1=0.01642  #Vertical displacement hinge 3, 1.642cm
+theta=26    #Degrees max deflection         26
+P=91700     #Force of actuator 1            91.7kN
+q=4530      #Distributed aerodynamic load   4.53kN/m
