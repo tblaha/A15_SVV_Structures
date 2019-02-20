08360bf16572e7de12dd9d731161720255e28d0a
tblaha <till.blaha@web.de>
2019-02-20 11:26:04 +0100

    Discretization Debugged, Reaction Force commented

--- discretization.py
+++ discretization.py
@@ -51,7 +51,7 @@ def discretizeCrossSection(h_a, c_a, n_st, A_st, t_sk, t_sp, y_c, z_c, booms_bet
     # booms_between*(n_st+1) +       # in each of the (n_st+1) segments there are booms_between booms
     # booms_between*spar_upscaling + # upscaling for the spar
     # 2                              # two additional booms at the ends of the spar
-    B = np.zeros((n_st + booms_between*(n_st+1+spar_upscaling) + 2, 5))
+    B = np.zeros((n_st + booms_between*(n_st+1+spar_upscaling), 5)) # works better without the +2
     
     
     
@@ -75,7 +75,7 @@ def discretizeCrossSection(h_a, c_a, n_st, A_st, t_sk, t_sp, y_c, z_c, booms_bet
     # length per boom segment (here, the booms in the spar need to be
     # subtracted since we want to know the length intervals in the skin, not
     # the spar as the spar is handled later)
-    length_per_boom_seg  = total_length/(len(B)-booms_between*spar_upscaling-2)
+    length_per_boom_seg  = total_length/(len(B)-booms_between*spar_upscaling) # works better without the -2
     
     # number of stiffeners and booms in the circular segment
     stiffs_quarter_circular = np.floor(sc_arc_length/2/length_per_stiff_seg)
@@ -396,7 +396,7 @@ def discretizeSpan(x_h1, x_h2, x_h3, d_a, l_a, nodes_between=50,ec=0.0001,offset
 
 # debugging
 
-# B=discretizeCrossSection(h_a, c_a, n_st, 1, t_sk, t_sp, 2, 3, 5)
-# for i in range(10,-1,-1):
+#B=discretizeCrossSection(h_a, c_a, n_st, 1, t_sk, t_sp, 0, -98, 3)
+#for i in range(10,-1,-1):
 #     B = discretizeCrossSection(h_a, c_a, n_st, t_st*(w_st+h_st-t_st), t_sk, t_sp, 0, -98, i)
 #     plotCrossSection(B)
