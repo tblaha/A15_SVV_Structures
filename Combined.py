b34d0e62feaef69c1d7bbdd2514169a2d8d7663a
DaanVerkooij <d@anverkooij.nl>
2019-02-20 10:29:28 +0100

    cell area thing

--- Combined.py
+++ Combined.py
@@ -22,12 +22,12 @@ span_ec=0.0001 #How close should the first point be to the point of interest
 span_offset=30 #How concentrated should the points be (Lower is higher concentration)
 cross_booms_between=20 #The amount of booms in the cross-section
 
-#Start with finding the centroid
-y_Bar,z_Bar=findCentroid()
+#Start with finding the centroid (small letter due to reference system)
+y_bar,z_bar=findCentroid()
 
 #Discretize spanwise
-Span_Discretization=discretizeSpan(span_nodes_between,span_ec,span_offset)
+span_discretization=discretizeSpan(span_nodes_between,span_ec,span_offset)
 
 #Discretize cross-section
-Cross_Discretization=discretizeCrossSection(h_a, c_a, n_st, A_st, t_sk, t_sp, y_Bar, z_Bar, booms_between)
+cross_discretization=discretizeCrossSection(h_a, c_a, n_st, A_st, t_sk, t_sp, y_bar, z_bar, cross=booms_between)
 
