584116037deacde53fcd21bd40093120636acec4
Mathieu <mathieudheer@gmail.com>
2019-02-20 12:00:05 +0100

    Upate

--- InternalLoads.py
+++ InternalLoads.py
@@ -9,30 +9,23 @@ from UniversalConstants import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def Macauly (xlocation, x1):
-    a = x1 - xlocation
+#Define a macauly function
+def Macauly (x1, x2):
+    a = x1 - x2
     if a < 0:
         a = 0 
-        
-    else:
-        a = x1 - xlocation
-        
     return a
-print(Macauly(2,6))
     
-def InternalShearForcey (xlocation):
-    SFy = xlocation * q * sin(radians(theta)) 
-    
-    return
-
 def InternalShearForcez (xlocation):
-    
-    
-    return
+    SFIz = Macauly(xlocation,0)* xlocation * q * sin(radians(theta)) + Macauly(xlocation,x_h1) * -F_z_1 + Macauly(xlocation,x_h2) * -F_z_2 + Macauly(xlocation,x_h3) * -F_z_3 + P_1 * Macauly(xlocation, x_h2 - d_a/2) * cos(theta) - P_2 * Macauly(xlocation, x_h2 - d_a/2) * cos(theta)
+    return SFIz
 
-def InternalMomentx (A):
-    
+def InternalShearForcey (xlocation):
+    SFIy = Macauly(xlocation,0)* xlocation * -q * cos(radians(theta)) + Macauly(xlocation,x_h1) * -F_z_1 + Macauly(xlocation,x_h2) * -F_z_2 + Macauly(xlocation,x_h3) * -F_z_3 + P_1 * Macauly(xlocation, x_h2 - d_a/2) * sin(theta) - P_2 * Macauly(xlocation, x_h2 - d_a/2) * sin(theta)    
+    return SFIy
+
+def InternalMomentx (xlocation):
+    MIx = cos(radians(theta)) * -q * (0.25*c_a - h_a/2) * Macauly(xlocation,0)* xlocation/2 + P_1 * cos(theta) * -h_a/2 * Macauly(xlocation, x_h2 - d_a/2)
     
     return
 
