d607959c68f6c5d04e11a7b3001bece4820eade1
Mathieu <mathieudheer@gmail.com>
2019-02-21 16:41:22 +0100

    Update4:41

--- InternalLoads.py
+++ InternalLoads.py
@@ -30,7 +30,10 @@ def InternalShearForcex (xlocation):
     return SFIx
 
 def InternalShearForcey (xlocation):
-    SFIy = (Macauly(xlocation,0)* xlocation * -q * cos(radians(theta))) + (Macauly(xlocation,x_h1) * -F_z_1) + (Macauly(xlocation,x_h2) * -F_z_2) + (Macauly(xlocation,x_h3) * -F_z_3) + (P_1 * Macauly(xlocation, x_h2 - d_a/2) * cos(theta)) - (P_2 * Macauly(xlocation, x_h2 + d_a/2) * cos(theta))
+    SFIy = (Macauly(xlocation,0)* xlocation * -q * cos(radians(theta)))\
+            + (Macauly(xlocation,x_h1) * -F_z_1) + (Macauly(xlocation,x_h2) * -F_z_2)\
+            + (Macauly(xlocation,x_h3) * -F_z_3) + (P_1 * Macauly(xlocation, x_h2 - d_a/2) * cos(theta)) \
+            - (P_2 * Macauly(xlocation, x_h2 + d_a/2) * cos(theta))
     return SFIy
 
 def InternalShearForcez (xlocation):
@@ -50,3 +53,46 @@ def InternalMomenty (xlocation):
 def InternalMomentz (xlocation): 
     MIz = (F_y_1 * Macaulyyy(xlocation,x_h1)) + (Macaulyyy(xlocation,x_h2) * F_y_2) + (Macaulyyy(xlocation,x_h3) * F_y_3) + (q * Macaulyyy(xlocation/2,0) * xlocation * sin(theta)) + (P_1* Macauly(xlocation, x_h2 - d_a/2) * sin(theta))  - (P_2 * Macauly(xlocation, x_h2 - d_a/2) * sin(theta))
     return MIz
+
+def PlotDiagrams():
+    import matplotlib.pyplot as plt
+    x = np.arange(0,2771,1)
+    
+    
+    #First row: plot 1-3
+    plt.subplot(231)
+    plt.plot(x,y1)
+    plt.title('Internal shear force x')
+    
+    plt.subplot(232)
+    plt.plot(x,y2)
+    plt.title('Internal shear foce y')
+    
+    plt.subplot(233)
+    plt.plot(x,y3)
+    #plt.axis([-2.*pi,2.*pi,-5.,5.]) # setting scale:xmin,xmax,ymin,ymax
+    plt.title('Internal shear force z')
+    
+    # Second row: plot 4-6
+    plt.subplot(234)
+    plt.plot(x,y4)
+    plt.title('Internal moment x')
+    
+    plt.subplot(235)
+    plt.plot(x,y5)
+    plt.title('Internal moment y')
+    
+    plt.subplot(236)
+    plt.plot(x,y6)
+    plt.title('Internal moment z')
+    
+    plt.show()
+    
+    return 0
+
+PlotDiagrams()
+
+
+
+
+
