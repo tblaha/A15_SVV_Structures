7d8d2b01502a3bdc4eb495989394a1090ee665e8
Patty <patricia.apostol@gmail.com>
2019-02-21 17:06:46 +0100

    Updated with moment contribution

--- ShearFlows.py
+++ ShearFlows.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 B=discretizeCrossSection(h_a, c_a, n_st, 1, t_sk, t_sp, 0, -98, 3)
 l_Skin_Curved=findCentroid()[2]
 
-def baseShearFlows(I_zz,I_yy,V_z,V_y,B_array,l_Skin_Curved):
+def baseShearFlows(I_zz,I_yy,V_z,V_y,B_array,l_Skin_Curved,A_1,A_2,):
     #This function takes in the MOI parmeters as well as internal shear forces and 
     #cross sectional boom discretization.
     #The output is a 2D array with all the base shear flows in each segment
@@ -49,6 +49,8 @@ def baseShearFlows(I_zz,I_yy,V_z,V_y,B_array,l_Skin_Curved):
     
     Line_Integral_qb = np.zeros((len(B_array[:,0]),4))
     
+    Moment_Integral_qb = np.zeros((len(B_array[:,0])),4)
+    
     #Initialize first cell
     ID_current=1
     ID_new = 1
@@ -61,6 +63,14 @@ def baseShearFlows(I_zz,I_yy,V_z,V_y,B_array,l_Skin_Curved):
     Line_Integral_qb_1=0.
     Line_Integral_qb_2=0.
     Line_Integral_qb_3=0.
+    
+    #Initialise all the moment contributions to zero. Note moments are taken about the centroid
+    #of the cross-section
+    Moment_Cont_qb_1_z=0.
+    Moment_Cont_qb_1_y=0.
+    Moment_Cont_qb_2_z=0.
+    Moment_Cont_qb_2_y=0.
+    Moment_Cont_qb_3=0.
         
     while ID_current<=3:
         
@@ -94,6 +104,10 @@ def baseShearFlows(I_zz,I_yy,V_z,V_y,B_array,l_Skin_Curved):
             Line_Integral_qb[i,0]=i+1
             Line_Integral_qb[i,3]=ID_current
             
+            #Updare Moment integral q_b:
+            Moment_Integral_qb[i,0]=i+1
+            Moment_Integral_qb[i,3]=ID_current
+            
             #Distance between each boom. 
             #FORMAT: / i number / y-direction / z-direction / ID /
             
@@ -111,18 +125,37 @@ def baseShearFlows(I_zz,I_yy,V_z,V_y,B_array,l_Skin_Curved):
             if (ID_current == 1):
                 Line_Integral_qb[i,1] = (np.multiply(Qb_y[i,1],B_Distance[i,1]))/(t_sk*G)
                 Line_Integral_qb[i,2] = (np.multiply(Qb_z[i,1],B_Distance[i,2]))/(t_sk*G)
+                Moment_Integral_qb[i,1]=(np.multiply(Qb_y[i,1],B_Distance[i,1],B_Distance[i,2]))
+                Moment_Integral_qb[i,2]=(np.multiply(Qb_z[i,1],B_Distance[i,2],B_Distance[i,1]))
+                
+                #Moment contribution will be negative for first skin
+                Moment_Cont_qb_1_z= -(Moment_Cont_qb_1_z+ Moment_Integral_qb[i,2])
+                Moment_Cont_qb_1_y= -(Moment_Cont_qb_1_y + Moment_Integral_qb[i,1])
+                
                 Line_Integral_qb_1 = Line_Integral_qb_1 + Line_Integral_qb[i,1] + Line_Integral_qb[i,2]  
+                
             
                 
             elif (ID_current == 2):
                 Line_Integral_qb[i,1] = (np.multiply(Qb_y[i,1],B_Distance[i,1]))/(t_sk*G)
                 Line_Integral_qb[i,2] = (np.multiply(Qb_z[i,1],B_Distance[i,2]))/(t_sk*G)
+                Moment_Integral_qb[i,1]=(np.multiply(Qb_y[i,1],B_Distance[i,1],B_Distance[i,2]))
+                Moment_Integral_qb[i,2]=(np.multiply(Qb_z[i,1],B_Distance[i,2],B_Distance[i,1]))
+                
+                #Moment contribution is positive for the second skin
+                Moment_Cont_qb_2_z= Moment_Cont_qb_2_z+ Moment_Integral_qb[i,2]
+                Moment_Cont_qb_2_y= Moment_Cont_qb_2_y + Moment_Integral_qb[i,1]
+                
                 Line_Integral_qb_2 = Line_Integral_qb_2 + Line_Integral_qb[i,1] + Line_Integral_qb[i,2]  
-                print (Line_Integral_qb_2)
+            
                 
             elif (ID_current == 3):
                 Line_Integral_qb[i,1] = (np.multiply(Qb_y[i,1],B_Distance[i,1]))/(t_sp*G)
                 Line_Integral_qb[i,2] = (np.multiply(Qb_z[i,1],B_Distance[i,2]))/(t_sp*G)
+                Moment_Integral_qb[i,1]=(np.multiply(Qb_y[i,1],B_Distance[i,1],B_Distance[i,2]))
+                
+                #Moment contribution is negative for spar 
+                Moment_Cont_qb_3 =  -(Moment_Cont_qb_3+Moment_Integral_qb[i,1])
                 Line_Integral_qb_3 = Line_Integral_qb_3 + Line_Integral_qb[i,1] + Line_Integral_qb[i,2]
             
             i=i+1
@@ -146,6 +179,16 @@ def baseShearFlows(I_zz,I_yy,V_z,V_y,B_array,l_Skin_Curved):
     qs0_2_Cell_2 = (1./(2.*Cell_Area2))*(((l_Skin_Curved)/(t_sk*G))+((h_a/(t_sp*G))))
     
     #Moment Equation (eq. 5.8)
+    #Contributions for the base shears are given by the values outputed within the second while loop
+    
+    #Contributions for the base shear flows can be given as a function of the areas:
+    
+    Moment_Cont_qs0_1=2*Cell_Area1*qs0_1
+    Moment_Cont_qs0_2=2*Cell_Area_2*qs0_2
+    
+    RHS_Moment_eq=Moment_Cont_qs0_1 + Moment_Cont_qs0_2 + Moment_Cont_qb_1_z +  Moment_Cont_qb_1_y + Moment_Cont_qb_2_z + Moment_Cont_qb_2_y + Moment_Cont_qb_3 
+    
+    
         
     
     
