24a1fdcc5c8d2664deef8b75f627e5bed1ac49d7
DaanVerkooij <d@anverkooij.nl>
2019-02-22 15:25:50 +0100

    added internalloads to combined.py

--- Combined.py
+++ Combined.py
@@ -23,8 +23,8 @@ span_nodes_between=50 #How many nodes between two points of interest
 span_ec=0.0001 #How close should the first point be to the point of interest
 span_offset=30 #How concentrated should the points be (Lower is higher concentration)
 booms_between=20 #The amount of booms between each centre
-
-
+plotBending=0
+plotInternal=1
 
 #Generate stiffener locations
 Stiffeners = generateStiffeners(h_a, c_a, n_st, A_st, t_sk, t_sp)
@@ -42,9 +42,9 @@ cross_disc=discretizeCrossSection(h_a, c_a, n_st, A_st, t_sk, t_sp, y_bar, z_bar
 I_zz,I_yy = MomentOfInertia(cross_disc)
 
 ##Get bending and reaction forces
-d_yz_vec, Fx, Fy, Fz, P = sampleBendingShape(span_disc, x_h1, x_h2, x_h3, p, d_a, q, theta, c_a, h_a, l_a, d_1, d_3, E, I_zz, I_yy)
-PlotBending=0
-if PlotBending==1:
+d_yz_vec, Fx, Fy, Fz, P_1 = sampleBendingShape(span_disc, x_h1, x_h2, x_h3, p, d_a, q, theta, c_a, h_a, l_a, d_1, d_3, E, I_zz, I_yy)
+
+if plotBending==1:
     fig, axs = plt.subplots(2, 1)
     axs[0].plot(span_disc, d_yz_vec[0,:])
     axs[1].plot(span_disc, d_yz_vec[1,:])
@@ -55,16 +55,34 @@ if PlotBending==1:
     plt.show()
     
 ##Get Internal Loads
-F_y_1 = Fy[0]
-F_y_2 = Fy[1]
-F_y_3 = Fy[2]
 
-F_z_1 = Fz[0]
-F_z_2 = Fz[1]
-F_z_3 = Fz[2]
+SFIx, SFIy, SFIz, MIx, MIy, MIz = getInternalLoads(span_disc,Fx, Fy, Fz, P_1)
 
-InternalShearForcey(Span_disc)
-
+if plotInternal==1:
+    plt.subplot(231)
+    plt.plot(span_disc,SFIx)
+    plt.title('Internal shear force x')
+    
+    plt.subplot(232)
+    plt.plot(span_disc,SFIy)
+    plt.title('Internal shear foce y')
+    
+    plt.subplot(233)
+    plt.plot(span_disc,SFIz)
+    plt.title('Internal shear force z')
+    
+    plt.subplot(234)
+    plt.plot(span_disc,MIx)
+    plt.title('Internal moment x')
+    
+    plt.subplot(235)
+    plt.plot(span_disc,MIy)
+    plt.title('Internal moment y')
+    
+    plt.subplot(236)
+    plt.plot(span_disc,MIz)
+    plt.title('Internal moment z')
+    plt.show ()
 ##Compute 
 
 
