66c8dcbcf5a2b3fe92a3f82bcf0b1047aa2b237b
Tuur Baldewijns <tuur.baldewijns@gmail.com>
2019-02-26 14:13:54 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ ShearFlowRibs.py
@@ -0,0 +1,259 @@
+import numpy as np
+from numpy.linalg import solve, lstsq, inv, eigvals
+from UniversalConstants import *
+import Centroid as C
+import discretization as d
+import matplotlib.pyplot as plt
+import Stiffeners as s
+
+def areaTriangle(point_1, point_2, point_3):
+	A_x = point_1[0]
+	A_y = point_1[1]
+	B_x = point_2[0]
+	B_y = point_2[1]
+	C_x = point_3[0]
+	C_y = point_3[1]
+	return np.abs(((A_x * (B_y - C_y)) + (B_x * (C_y - A_y)) + (C_x * (A_y - B_y)))/2.)
+
+def arctan(x, y):
+	if x == 0. and y > 0.: return np.pi/2.
+	elif x == 0. and y < 0.: return -np.pi/2.
+	else: return np.arctan(y/x)
+
+def shearFlowRib(B, P_1=None, P_2=None, F_z=None, F_y=None):
+	'''
+	INPUTS:
+	- B:
+	The boom discretization of the cross-section produced by the
+	discretizeCrossSection() function in the discretization.py module.
+	
+	- P_1:
+	EXPLANATION
+	
+	- P_2:
+	EXPLANATION
+	
+	- F_x:
+	EXPLANATION
+	
+	- F_y:
+	EXPLANATION
+	
+	OUTPUTS:
+	- The shearflow in the web of the nose of the aileron.
+	Defined positive counter-clockwise.
+	- The shearflow in the web of the back of the aileron.
+	Defined positive counter-clockwise.
+	'''
+	
+	'''
+	plt.ion()
+	plt.grid()
+	plt.tight_layout()
+	for line in discr:
+		print(line)
+		if line[4] == 1: plt.plot(line[1], line[0], 'bo')
+		elif line[4] == 2: plt.plot(line[1], line[0], 'go')
+		elif line[4] == 3: plt.plot(line[1], line[0], 'ro')
+		else: print('Cell ID not recognized.')
+		plt.show()
+		plt.pause(0.01)
+	print('There are %i points in the cross-section discretization.' % len(discr))
+	plt.ioff()
+	plt.show()
+	'''
+
+	print('There are %i points in the cross-section discretization.' % len(B))
+
+	### This thing produces the array of point coordinates of the points on the circumfarence of the rib.###
+	# Define the point types.
+	nose = 1
+	tail = 2
+	spar = 3
+	points = []
+	points.append([0., h_a/2., spar])
+	for point in B:
+		if point[4] == 1: points.append([point[1], point[0], nose])
+	points.append([0., -h_a/2., spar])
+	for point in B:
+		if point[4] == 2: points.append([point[1], point[0], tail])
+	points = np.array(points)
+	#print(points)
+	
+	plot = False
+	if plot:
+		plt.plot(points.transpose()[0], points.transpose()[1], 'bo')
+		plt.grid()
+		plt.tight_layout()
+		plt.show()
+	print('There are %i points on the circumference.' % len(points))
+	### END ###
+
+	### Here we construct the matrix A. This matrix is only dependend on the geometry of the cross-section (the boom positions of the booms in this case). ###
+	points_transpose = points.transpose()
+	sparcap_top = points_transpose[1].argmax()
+	sparcap_bot = points_transpose[1].argmin()
+
+	two_point_areas = np.zeros(len(points))
+	for i in range(sparcap_bot):
+		angle_1 = arctan(points[i][0], points[i][1])
+		angle_2 = arctan(points[i+1][0], points[i+1][1])
+		angle = np.abs(angle_1 - angle_2)
+		area_circle = (np.pi * (h_a**2))/4.
+		area_pie = (angle/(2. * np.pi)) * area_circle
+		area_triangle = areaTriangle([0., 0.], points[i], points[i+1])
+		area_moon = area_pie - area_triangle
+		two_point_areas[i] = area_moon
+	
+	bottom_tail = points_transpose[0].argmin()
+	
+	area_tail_triangle = np.abs(points[bottom_tail][1])*((c_a - (h_a/2.)) - np.abs(points[bottom_tail][0]))
+	
+	two_point_areas[bottom_tail] = area_tail_triangle
+	
+	
+	plot = False
+	if plot:
+		plt.ion()
+		plt.grid()
+		plt.tight_layout()
+		for z, y, type in points:
+			if type == spar: plt.plot(z, y, 'ro')
+			elif type == nose: plt.plot(z, y, 'bo')
+			elif type == tail: plt.plot(z, y, 'go')
+			else: print('Type not recognized!')
+			plt.show()
+			plt.pause(0.01)
+		plt.ioff()
+		plt.show()
+	
+	
+	#print(two_point_areas)
+
+	A = np.zeros((len(points), len(points)))
+	A = np.matrix(A)
+
+	for row in range(len(points)):
+		for col in range(len(points)-1):
+			area = areaTriangle(points[row], points[col], points[col + 1]) + two_point_areas[col]
+			A[row, col] = -2. * area
+		col = len(points)-1
+		area = areaTriangle(points[row], points[len(points)-1], points[0]) + two_point_areas[len(points)-1]
+		A[row, len(points)-1] = -2. * area
+	'''
+	for i in range(len(points)-1):
+		A[sparcap_top, i] = points[i+1][0] - points[i][0]
+		A[sparcap_bot, i] = points[i+1][1] - points[i][1]
+	A[sparcap_top, len(points)-1] = points[0][0] - points[len(points)-1][0]
+	A[sparcap_bot, len(points)-1] = points[0][1] - points[len(points)-1][1]
+	'''
+	eigen_values = eigvals(A)
+	condition_number = max(eigen_values)/min(eigen_values)
+	print(max(eigen_values))
+	print(min(eigen_values))
+	print('The eigenvalue of the matrix A is %f.' % condition_number)
+	
+	plot = False
+	if plot:
+		plt.imshow(A,interpolation='none')#,cmap='binary')
+		plt.colorbar()
+		plt.show()
+	
+	#print(A)
+	### END ###
+
+	### This bit creates the b vector. The b vector is dependend on the load case. ###
+	#F_z = 600.
+	#F_y = 600.
+	
+	b = np.zeros(len(points))
+	for i in range(len(points)):
+		b[i] = (-0. + points[i][1])*F_z + (0. - points[i][0])*F_y
+
+	#b[sparcap_top] = F_z
+	#b[sparcap_bot] = F_y
+
+	#print(b)
+	### END ###
+	
+	q = lstsq(A, b)
+	q = q[0]
+	
+	#q = solve(A, b)
+	
+	#A_inv = inv(A)
+	#q = np.dot(A_inv, b)
+	#q = np.array(q)[0]
+	
+	plot = True
+	if plot:
+		plt.title('Number of points: ' + str(len(points)))
+		plt.plot(range(len(points)), q, color='lightgray')
+		for i in range(len(points)):
+			if points[i][2] == nose: plt.plot(i, q[i], 'bo')
+			elif points[i][2] == spar: plt.plot(i, q[i], 'ro')
+			elif points[i][2] == tail: plt.plot(i, q[i], 'go')
+			else: print('Type not recognized!')
+		plt.tight_layout()
+		plt.grid()
+		plt.show()
+	
+	### Here we check whether the sum of the shear forces matches the applied forces. ###
+	fz = 0
+	fy = 0
+	for i in range(len(points)-1):
+		fz += q[i]*(points[i+1][0] - points[i][0])
+		fy += q[i]*(points[i+1][1] - points[i][1])
+	fz += q[len(points)-1]*(points[0][0] - points[len(points)-1][0])
+	fy += q[len(points)-1]*(points[0][1] - points[len(points)-1][1])
+	print('The sum of the shearforces in the z direction: fz = %f.' % fz)
+	print('The sum of the shearforces in the y direction: fy = %f.' % fy)
+	### END ###
+	
+	### Determine the shearflows in the ribs. ###
+	q_1 = 0
+	q_2 = 0
+	for i in range(len(points)-1):
+		if points[i][2] == nose or points[i+1][2] == nose:
+			q_1 += q[i]*(points[i+1][1] - points[i][1])
+		if points[i][2] == tail or points[i+1][2] == tail:
+			q_2 += q[i]*(points[i][1] - points[i+1][1])
+	q_2 += q[len(points)-1]*(points[len(points)-1][1] - points[0][1])
+	q_1 = q_1/h_a
+	q_2 = q_2/h_a
+	### END ###
+	
+	return q_1, q_2
+
+print('Triangles tests:')
+print(areaTriangle([0., 0.], [1., 0.], [0., 1.]))
+print(areaTriangle([1., 0.], [0., 0.], [0., 1.]))
+print(areaTriangle([-1., 0.], [1., 0.], [0., 1.]))
+print(areaTriangle([1., 0.], [-1., 0.], [0., 1.]))
+print()
+
+# This bit was used for testing.
+q_1_list = []
+q_2_list = []
+Stiffeners = s.generateStiffeners(h_a, c_a, n_st, A_st, t_sk, t_sp, Ybar_st, 0.)
+Y_bar, Z_bar = C.findCentroid(Stiffeners)
+booms_between_list = [0, 1, 5, 10, 20, 30, 40, 50]
+booms_between_list = range(0, 55, 5)
+booms_between_list = [0, 1, 5, 10, 20]
+#booms_between_list = [0, 1, 2, 5, 10, 20, 50, 100]
+booms_between_list = range(21)
+#booms_between_list = [0]
+for i in booms_between_list:
+	booms_between = i
+	B = d.discretizeCrossSection(h_a, c_a, 3, A_st, t_sk, t_sp, Y_bar, Z_bar, booms_between, Ybar_st, 0.)
+	q_1, q_2 = shearFlowRib(B, F_z=19390., F_y=8397.)
+	q_1_list.append(q_1)
+	q_2_list.append(q_2)
+	print('The calculated shearflow in the nose web: q_1 = %f.' % q_1)
+	print('The calculated shearflow in the nose web: q_2 = %f.' % q_2)
+	print()
+plt.plot(booms_between_list, q_1_list, 'b-o', label='q_1')
+plt.plot(booms_between_list, q_2_list, 'g-o', label='q_2')
+plt.grid()
+plt.tight_layout()
+plt.show()
